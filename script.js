1c541689eff5b546a87e4a3fa8b7df6668d7ea4e
SijanBhandari17 <uhisijan@gmail.com>
2024-11-23 23:31:38 +0545

    This is preety good

--- script.js
+++ script.js
@@ -1,46 +1,70 @@
-const arr1 = [];
-const divArray1 =[];
-const divArrayOne = document.createElement('div');
 const container  = document.querySelector(".container");
+let individualContainer;
+const arr = [];
+for(let i = 0;i < 16;i++){
 
-for(let i = 1;i <= 16;i++){
+    arr.push("div" + (i + 1));
 
-    arr1.push("div" + i);
+}
+
+
+const button10 = document.querySelector(".button10");
+const button20 = document.querySelector(".button20");
+const button30 = document.querySelector(".button30");
+
+
+
+
+const divArray = [];
+createGrid(16,16);
+
+function createGrid(rows,columns){
+
+    for(let i = 0;i <rows;i++){
+        
+        individualContainer = document.createElement('div');
+        divArray[i] = [];
+        
+        
+        for(let j = 0;j < columns;j++){
+            
+            const div = document.createElement('div');
+            div.classList.add(`row${j}`);
+            divArray[i][j] = div;
+
+        }
+        divArray[i].map((items)=> individualContainer.appendChild(items));
+        individualContainer.classList.add("individualContainer");
+        container.appendChild(individualContainer);
+    }
+    const items = document.querySelectorAll('[class^="row"]');
+    
+    items.forEach(row => {
+        row.addEventListener('mouseenter', () => {
+            row.classList.add('hover-active');
+        });
+        
+        
+    });
 
 }
     
-arr1.map((item)=> {
-
-    const div = document.createElement('div');
-    div.textContent = item;
-    div.classList.add("row1");
-    divArray1.push(div);
-});
-
-
-
-divArray1.map((item) => divArrayOne.appendChild(item));
-divArrayOne.classList.add("divArrayOne");
-container.appendChild(divArrayOne);
-
-
-const arr2= [];
-const divArray2 =[];
-
-for(let i = 1;i <= 16;i++){
-
-    arr2.push("div" + i);
-
-}
     
-arr2.map((item)=> {
+button10.addEventListener("click",()=>{
+    container.removeChild(individualContainer);
+    container.innerHTML = '';
+    createGrid(10,10)
+    
+})
+button20.addEventListener("click",()=>{
+    container.removeChild(individualContainer);
+    container.innerHTML = '';
+    createGrid(20,20);
+})
 
-    const div = document.createElement('div');
-    div.textContent = item;
-    div.classList.add("row2");
-    divArray2.push(div);
-});
-
-
-divArray2.map((item) => container.appendChild(item));
+button30.addEventListener("click",()=>{
+    container.removeChild(individualContainer);
+    container.innerHTML = '';
+    createGrid(30,30);
+})
 
